7ccf95ffc4f7c67767902c89726a66fa8fdf598a
hemloeth <rustamali3488@gmail.com>
2025-12-23 16:57:18 +0530

    changes

--- app/components/sareeCard.js
+++ app/components/sareeCard.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Star } from "lucide-react";
 
 export default function LinenSarees({ images = [], title, price, originalPrice, discountedPercentage, type = [], rating }) {
     return (
-        <div className="w-[325px] h-[580px] border-[0.5px] border-[#dfdfdf] group relative overflow-hidden rounded-[46px] " style={{
+        <div className="bg-[#eeeeee] w-[325px] h-[580px] border-[0.5px] border-[#dfdfdf] group relative overflow-hidden rounded-[46px] " style={{
             marginTop: 20
         }}>
             <div>
@@ -31,39 +31,43 @@ export default function LinenSarees({ images = [], title, price, originalPrice, 
 
             </div>
             <div className="hover:shadow-[0_-20px_24px_rgba(0,0,0,0.1)] absolute bg-white h-[176px] border-[0.5px] border-[#dfdfdf] bottom-[10px] left-[10px] right-[10px] pb-[10px] px-[10px] rounded-[36px]">
-                <div className="absolute" style={{ top: 20, left: 20, right: 20, bottom: 10 }}>
+                <div className="absolute" style={{ top: 20, left: 20, right: 20, bottom: 20 }}>
                     <div className="flex justify-between px-[10px] mt-[10px]">
-                    <p className="text-[12px] text-color-[#9c9c9c]">{type}</p>
-                    <p className="text-[12px]">{rating}</p>
-                </div>
-                <div className="">
-                    <h2 className="text-[18px] text-black font-semibold mt-[10px]" >{title}</h2>
-                </div>
+                        <p className="text-[12px] text-color-[#6e6e6e]">{type}</p>
+                        <p className="flex items-center gap-[4px] text-[12px]">
+                            <Star size={12} className="shrink-0" fill="#30c000"
+                                stroke="#30c000" />
+                            <span className="text-[12px] tex-[#9e9e9e]">{rating}</span>
+                        </p>
 
-                <div className="mb-0" style={{marginTop:10}}>
-                    <span>Price</span>
-                    <p className="text-[12px] line-through">{originalPrice}</p>
-
-                </div>
-                <div className="absolute bottom-0 left-0 right-0 flex justify-between items-center px-[10px]">
-                    <span className="text-[16px] font-semibold text-[#30c000]">₹{price}</span>
-
-                    <div className="flex gap-2 items-center">
-                        <button type="button" className="w-[38px] h-[38px] rounded-[16px] border border-[#dfdfdf] bg-transparent flex items-center justify-center">
-                            <ShoppingCart size={18} color="#000" />
-                        </button>
-
-                        <button
-                            type="button"
-                            className="bg-[#30c000] text-white text-[16px] font-medium px-[20px] py-[10px] w-[70px] h-[38px] rounded-[16px] flex items-center justify-center"
-                        >
-                            Buy
-                        </button>
                     </div>
-                </div>
+                    <div style={{ marginTop: 2 }}>
+                        <h3 className="text-[18px] font-semibold text-black">{title}</h3>
+                    </div>
 
+                    <div className="flex justify-between" style={{ marginTop: 20 }}>
+                        <div className="flex flex-col gap-[0px]">
+                            <p className="text-[12px] text-[6e6e6e]">Price</p>
+                            <span className="text-[12px] text-[#9e9e9e] line-through ml-2">${originalPrice}</span>
+                            <span className="text-[16px] text-[#30c000] font-semibold">${price} </span>
+
+                        </div>
+                        <div className="flex gap-[10px]" style={{marginTop:25}}>
+                            <button type="button" className="w-[38px] h-[38px] rounded-[16px] border border-[#dfdfdf] bg-transparent flex items-center justify-center">
+                                <ShoppingCart size={18} color="#000" />
+                            </button>
+
+                            <button
+                                type="button"
+                                className="bg-[#30c000] text-white text-[16px] font-medium px-[20px] py-[10px] w-[70px] h-[38px] rounded-[16px] flex items-center justify-center"
+                            >
+                                Buy
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
